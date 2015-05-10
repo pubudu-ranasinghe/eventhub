01f44a76d882303200ec6dbf2533c1fcbab5335c
Pubudu Ranasinghe <pubudutr@gmail.com>
2015-05-10 22:27:14 +0530

    fix date range display

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,4 +2,17 @@ module EventsHelper
   def format_time_field(time)
     time.strftime("%I:%M %p") unless time.blank?
   end
+
+  def print_event_date
+    s_date = @event.start_date.strftime("%b %d")
+    e_date = @event.end_date.strftime("%b %d")
+    s_time = @event.start_time.strftime("%I:%M%p")
+    e_time = @event.end_time.strftime("%I:%M%p")
+
+    if @event.start_date == @event.end_date
+      content_tag(:h3, "#{s_date} from #{s_time} to #{e_time}")
+    else
+      content_tag(:h3, "#{s_date} - #{s_time} to #{e_date} - #{e_time}")
+    end
+  end
 end
