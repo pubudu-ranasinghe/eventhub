dbad5becd78a9e7407321baa9686bc37519f4ef7
Pubudu Ranasinghe <pubudutr@gmail.com>
2015-05-16 09:40:45 +0530

    rollback and fix :/

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150510132918) do
+ActiveRecord::Schema.define(version: 20150513160027) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 20150510132918) do
     t.date     "end_date"
     t.time     "end_time"
     t.integer  "user_id"
+  end
+
+  create_table "registrations", force: :cascade do |t|
+    t.integer  "event_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
