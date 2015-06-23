e9b60020ac7a4de3f47ce59dbcacbaddf6ed987f
Pubudu Ranasinghe <pubudutr@gmail.com>
2015-06-23 21:15:01 +0530

    minor styling

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -4,7 +4,7 @@ module RegistrationsHelper
     if e.registrations.where(user: current_user).any?
       content_tag(:h4, "You are registered", class: 'registered-notice')
     else
-      link_to 'Register', new_event_registration_path(@event), class: 'btn btn-primary'
+      link_to 'Register', new_event_registration_path(@event), class: 'btn btn-primary btn-block'
     end
   end
 
