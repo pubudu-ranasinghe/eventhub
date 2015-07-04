de40ab8720f7fafb64edea6e21114848886161db
Pubudu Ranasinghe <pubudutr@gmail.com>
2015-07-04 07:58:48 +0530

    confirmation fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,7 @@ class User < ActiveRecord::Base
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name   # assuming the user model has a name
       # user.image = auth.info.image # assuming the user model has an image
+      user.skip_confirmation!
     end
   end
 
