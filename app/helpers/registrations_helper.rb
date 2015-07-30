f9835db1439786b222738f5c804aaed287a9b371
Pubudu Ranasinghe <pubudutr@gmail.com>
2015-07-30 17:43:26 +0530

    styling dashboard

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -18,12 +18,12 @@ module RegistrationsHelper
     if e.approved == false
       content_tag(:h4, "Registrations Unavailable", class: 'registered-notice')
     elsif e.published == true
-      if e.users.count < 0
+      if e.no_of_registrations <= 0
         # return nothing
       elsif e.registrations.where(user: current_user).any?
         content_tag(:h4, "You are registered", class: 'registered-notice')
       else
-        if e.users.count > 0
+        if e.users.count >= 0
           content_tag(:div) do
             concat link_to 'Register for Free!', new_event_registration_path(@event), class: 'btn btn-primary'
             concat content_tag(:p, "Only " +(e.no_of_registrations-e.users.count).to_s+ " spots left", class: 'spots-left')
